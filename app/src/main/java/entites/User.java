c5c2443b83cd42ccf4acfbf1c725d2fb2a539f94
Jayasankar R <jayasankarr@Jayasankars-MacBook-Air.local>
2025-05-25 17:02:40 +0530

    Initial commit

---
+++ app/src/main/java/entites/User.java
@@ -0,0 +1,13 @@
+package entites;
+
+public class User {
+    private String name;
+
+    private String password;
+
+    private String hashpassword;
+
+    private List<Ticket>ticketsBooked;
+
+    private String userId;
+}
